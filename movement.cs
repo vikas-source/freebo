79db985792384600234bbf2831d63c40dce94076
vikas-source <58158590+vikas-source@users.noreply.github.com>
2020-01-22 13:44:03 +0530

    Add files via upload

--- movement.cs
+++ movement.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    float jump = 100f;
-    bool isgrounded;
+    public float jump ;
     bool isdead = false;
     Rigidbody2D rb;
     Animator anim;
     BoxCollider2D bc2d;
     SpriteRenderer sr;
+    //GroundCheck 
+    private bool isgrounded;
+    public Transform groundcheck;
+    public float radiuscheck;
+    public LayerMask whatisgrounded;
+    //Extra Jumps
+    private int extraJumps;
+    public int extraJumpValue;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,42 +27,65 @@ public class movement : MonoBehaviour
         anim = GetComponent<Animator>();
         bc2d = GetComponent<BoxCollider2D>();
         sr = GetComponent<SpriteRenderer>();
+        extraJumps = extraJumpValue;
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        isgrounded = Physics2D.OverlapCircle(groundcheck.position, radiuscheck, whatisgrounded);
+
+        //Move right >>
+        if (Input.GetKey(KeyCode.D))
+        {
+            sr.flipX = false;
+            rb.velocity = new Vector3(1.2f, 0f, 0f);
+            anim.SetBool("isRun", true);
+        }
+
+        //Move left <<
+        if (Input.GetKey(KeyCode.A))
+        {
+            sr.flipX = true;
+            rb.velocity = new Vector3(-1.2f, 0f, 0f);
+            anim.SetBool("isRun", true);
+        }
+
+
+    }
+    void Update()
+    { 
       if (isdead ==false)
-       {
-          
-
-          if (Input.GetKey(KeyCode.D))
+       {  
+            if(isgrounded == true)
             {
-                rb.velocity = new Vector3(4f, 0f, 0f);
-                anim.SetBool("isRun", true);
-                sr.flipX = false;
+                extraJumps = extraJumpValue;
             }
 
-          if(Input.GetKey(KeyCode.A))
-            {
-                rb.velocity = new Vector3(-4f, 0f, 0f);
-                anim.SetBool("isRun", true);
-                sr.flipX = true;
-            }
-
-          if(Input.GetKey(KeyCode.Space) && isgrounded == true)
+            if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
             {
                 rb.AddForce(Vector2.up * jump);
+                extraJumps--;
                 anim.SetBool("isjump", true);
                 anim.SetBool("isRun", false);
                 isgrounded = false;
             }
+            else if(Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isgrounded == true)
+            {
+                rb.AddForce(Vector2.up * jump);
+            }
+            
+            // Idle condition
+            if (Input.anyKey == false)
+            {
+                anim.SetBool("isRun", false);
+                anim.SetBool("isjump", false);
+             }
+          
        }
+      
     }
 
+
     
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        isgrounded = true;
-    }
 }
